df0610a93a9e9ce1a85681699f49ef6c5e27e055
João Correia <correia.jafonso@gmail.com>
2021-08-12 01:30:35 +0100

    Added datetime to tasks

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import datetime
 
 
 class Task(models.Model):
     description = models.CharField(max_length=255)
+    date = models.DateTimeField(default=datetime.datetime.utcnow)
     last_update = models.DateTimeField(auto_now=True)
     is_done = models.BooleanField(default=False)
 
